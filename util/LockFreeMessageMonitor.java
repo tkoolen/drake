1d797c3e4de27eda1c13ac37f6708de5d380a482
Twan Koolen <tkoolen@csail.mit.edu>
2015-05-10 14:34:40 -0400

    Made LockFreeMessageMonitor able to handle multiple channels.

--- util/LockFreeMessageMonitor.java
+++ util/LockFreeMessageMonitor.java
@@ -1,6 +1,10 @@
 package drake.util;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import lcm.lcm.LCM;
 import lcm.lcm.LCMDataInputStream;
@@ -8,15 +12,20 @@ import lcm.lcm.LCMSubscriber;
 
 public class LockFreeMessageMonitor implements LCMSubscriber {
 
-  ConcurrentCopier<LCMMessageData> copier;
-  LCMMessageData last_returned_data = null;
+  private final Map<String, ConcurrentCopier<LCMMessageData>> copiers = new ConcurrentHashMap<String, ConcurrentCopier<LCMMessageData>>();
+  private final Map<String, LCMMessageData> last_returned_data = new HashMap<String, LCMMessageData>();
   
   public LockFreeMessageMonitor() {
-    copier = new ConcurrentCopier<LCMMessageData>(new LCMMessageDataBuilder());
   }
 
   @Override
   public void messageReceived(LCM lcm, String channel, LCMDataInputStream ins) {
+    ConcurrentCopier<LCMMessageData> copier = copiers.get(channel);
+    if (copier == null) {
+      copier = new ConcurrentCopier<LockFreeMessageMonitor.LCMMessageData>(new LCMMessageDataBuilder());
+      copiers.put(channel, copier);
+    }
+    
     LCMMessageData data = copier.getCopyForWriting();
     int available = ins.available();
     if (data.byte_array == null || data.byte_array.length != available) { // TODO: is it OK to have a byte array that is too large?
@@ -31,14 +40,21 @@ public class LockFreeMessageMonitor implements LCMSubscriber {
     copier.commit();
   }
 
-  public byte[] getMessage() {
-    LCMMessageData data = copier.getCopyForReading();
-    if (data == null || data == last_returned_data)
-      return null;
-    else {
-      last_returned_data = data;
-      return data.byte_array;
+  /*
+   * Assumed to be called from a single thread
+   */
+  public Map<String, byte[]> getMessages() {
+    Map<String, byte[]> ret = new HashMap<String, byte[]>();
+    for (String channel : copiers.keySet()) {
+      ConcurrentCopier<LCMMessageData> copier = copiers.get(channel);
+      LCMMessageData data = copier.getCopyForReading();
+      if (data != null && data != last_returned_data.get(channel)) {
+        ret.put(channel, Arrays.copyOf(data.byte_array, data.byte_array.length));
+        last_returned_data.put(channel, data);
+      }
     }
+    
+    return ret;
   }
 
   private static class LCMMessageData {
