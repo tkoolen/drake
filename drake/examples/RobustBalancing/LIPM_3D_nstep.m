ef7aed9d195ea71ce38b675ebf9a7ab641c54087
Twan Koolen <koolen.twan@gmail.com>
2015-11-30 14:25:50 -0500

    Copy over some comments from 0-step. Start merging 0-step into n-step.

--- drake/examples/RobustBalancing/LIPM_3D_nstep.m
+++ drake/examples/RobustBalancing/LIPM_3D_nstep.m
@@ -18,17 +18,20 @@ end
 data=load(sprintf('V%d_LIPM',n-1));
 V0 = data.Vsol;
 
-prog = spotsosprog;
-degree = 6;
+% solution method settings
+degree = 6; % degree of V,W
+do_backoff = false; % solve once, then remove cost function and re-solve with cost as constraint (to improve numerical conditioning)
+time_varying = true; % Let V depend on t--probably want it true for this problem class
+R_diag = [2 2 2 2]; % state space ball
 
-do_backoff = false;
-time_varying = true;
-
+% model parameters
 T = .3;  % Step-time
 g = 10;  % gravity acceleration
 z_nom = 1; % nominal center of mass height
 step_max = .7; % max step distance
-dN = captureLimit(T, 0, step_max, z_nom, g, n); % theoretical max ICP distance
+
+% Create SOS program
+prog = spotsosprog;
 
 [prog,t]=prog.newIndeterminate('t',1);
 [prog,q]=prog.newIndeterminate('q',2);
@@ -37,40 +40,46 @@ dN = captureLimit(T, 0, step_max, z_nom, g, n); % theoretical max ICP distance
 x = [q;v];
 
 
+% Create polynomials V(t,x) and W(x)
 if time_varying
   V_vars = [t;x];
 else
   V_vars = x;
 end
-
 W_vars = x;
-
 [prog,V] = prog.newFreePoly(monomials(V_vars,0:degree));
 [prog,W] = prog.newFreePoly(monomials(W_vars,0:degree));
 
+% LIPM dynamics
 f = [v;q*g/z_nom];
 
 % time rescaling
+% dx/dtau = dx/dt * dt/dtau = dx/dt*T
 T_unscaled = T;
 f = f*T;
 T = 1;
 
 Vdot = diff(V,x)*f + diff(V,t);
 
+% goal region
+if n > 0
+  % jump equation
+  % control input changes q only
+  % qp = qm - u
+  xp = [q-u;v];
 
-% from Koolen et. al IJRR
-% regions should depend on the instantaneous capture point
-r_ic = q + v*sqrt(z_nom/g);
+  % for n > 0, goal region is based off V from 0-step model
+  % V0p(x) = V0(0,xp)
+  V0p = subs(V0,[x;t],[xp;0]);
+else
+  % use a small radius around the origin
+  goal_radius = .01; % TODO: setting
+  V0p = goal_radius^2 - x'*x;
+end
 
-%% jump equation
-% control input changes q only
-% qp = qm - u 
-xp = [q-u;v];
 
-% goal region is based off V from 0-step model
-% V0p(x) = V0(0,xp)
-V0p = subs(V0,[x;t],[xp;0]);
-
+A = diag(1./(R_diag.^2));
+h_X = 1 - x'*A*x;
 
 % 4 SOS constraints
 % (1) V(T,x) >= 0 for x in goal region
@@ -78,11 +87,6 @@ V0p = subs(V0,[x;t],[xp;0]);
 % (3) W(x) >= 0 for x in X
 % (4) W(x) >= V(0,x) + 1 for x in X
 sos = [(subs(V,t,T))*(1+x'*x + u'*u);-Vdot; W;W - subs(V,t,0) - 1];
-
-R_diag = [2 2 2 2];
-A = diag(1./(R_diag.^2));
-
-h_X = 1 - x'*A*x;
 
 % add goal region and h_X constraints
 [prog, sos(1)] = spotless_add_sprocedure(prog, sos(1), V0p,[W_vars;u],2);
@@ -112,12 +116,12 @@ options.do_fr = true;
 sol = prog.minimize(cost,@spot_mosek,options);
 
 if do_backoff
-  
+  % resolve problem with cost replaced by a constraint
   prog = prog.withPos(sol.eval(cost)*1.01 - coeff*l);
   sol = prog.minimize(0,@spot_mosek,options);
 end
 
-%% New-style plotting
+%% plotting
 Vsol = sol.eval(V);
 Wsol = sol.eval(W);
 sub_vars = [q(2);v(2);t];
@@ -126,9 +130,21 @@ plot_vars = [q(1);v(1)];
 
 figure(1)
 contourSpotless([Wsol;h_X],plot_vars(1),plot_vars(2),[-R_diag(1) R_diag(1)],[-R_diag(2) R_diag(2)],sub_vars,sub_val,[1 0],{'b','r'});
+xlabel('q_1')
+ylabel('v_1')
+title('W(x)')
+
+
+% from Koolen et. al IJRR
+% regions should depend on the instantaneous capture point
+r_ic = q + v*sqrt(z_nom/g);
+dN = captureLimit(T_unscaled, 0, step_max, z_nom, g, n); % theoretical max ICP distance
 
 figure(n*10+2)
 contourSpotless([Vsol;h_X;r_ic'*r_ic],plot_vars(1),plot_vars(2),[-R_diag(1) R_diag(1)],[-R_diag(2) R_diag(2)],sub_vars,sub_val,[0 0 dN^2],{'b','r','g'});
+xlabel('q_1')
+ylabel('v_1')
+title('V(0,x)')
 
 %%
 save(sprintf('V%d_LIPM',n),'Vsol')
