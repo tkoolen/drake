57f1710cc81c937841be74cbb3ceca83bf5055fe
Twan Koolen <koolen.twan@gmail.com>
2015-12-01 00:00:44 -0500

    Should be plotting W, not V.

--- drake/examples/RobustBalancing/henrionUnivariateCubicExample.m
+++ drake/examples/RobustBalancing/henrionUnivariateCubicExample.m
@@ -7,17 +7,17 @@ goal_radius = 0.01;
 target = @(x) goal_radius^2 - x'*x;
 n = 0;
 options.plotfun = @plotfun;
-
-options.degree = 16;
+options.degree = 32;
 
 nStepCapturabilitySOS(model, T, R_diag, target, n, options)
 end
 
 function plotfun(n, Vsol, Wsol, h_X, R_diag, t, x)
+% figure 1 in http://arxiv.org/pdf/1208.1751.pdf
 xs = linspace(-1, 1, 100);
-Vs = zeros(length(xs));
+Ws = zeros(length(xs));
 for i = 1 : length(xs)
-  Vs(i) = subs(Vsol, [x; t], [xs(i); 0]);
+  Ws(i) = subs(Wsol, [x; t], [xs(i); 0]);
 end
-plot(xs, Vs);
+plot(xs, Ws);
 end
