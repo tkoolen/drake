e3acdcf73d327f09727d3cd52f620ebb6e934abc
Twan Koolen <koolen.twan@gmail.com>
2015-12-24 14:29:02 +0100

    More work on WrappableSystem.

--- drake/systems/WrappableSystem.h
+++ drake/systems/WrappableSystem.h
@@ -34,6 +34,7 @@ namespace Drake {
 
     virtual ~WrappableSystemInterface() {};
     virtual StateVector<double> dynamics(const double& t, const StateVector<double>& x, const InputVector<double>& u) const = 0;
+    virtual OutputVector<double> output(const double& t, const StateVector<double>& x, const InputVector<double>& u) const = 0;
     virtual bool isTimeVarying() const = 0;
     virtual bool isDirectFeedthrough() const = 0;
   };
@@ -47,9 +48,16 @@ namespace Drake {
           x_sys = x;
           typename SystemType::template InputVector<double> u_sys;
           u_sys = u;
-          return toEigen(system_ptr->dynamics(t, x_sys, u_sys));
+          auto xdot_sys = system_ptr->dynamics(t, x_sys, u_sys);
+          return toEigen(xdot_sys);
         }),
-//      output([=](double t, const StateVector<double> &x) { return system_ptr->output(t, x); }),
+        output_double([=](double t, const StateVector<double> &x, const InputVector<double> &u) {
+          typename SystemType::template StateVector<double> x_sys;
+          x_sys = x;
+          typename SystemType::template InputVector<double> u_sys;
+          u_sys = u;
+          return toEigen(system_ptr->output(t, x_sys, u_sys));
+        }),
         is_time_varying([=] { return system_ptr->isTimeVarying(); }),
         is_direct_feedthrough([=] { return system_ptr->isDirectFeedthrough(); }) {
       // empty
@@ -59,6 +67,10 @@ namespace Drake {
 
     virtual StateVector<double> dynamics(const double& t, const StateVector<double>& x, const InputVector<double>& u) const override {
       return dynamics_double(t, x, u);
+    }
+
+    virtual OutputVector<double> output(const double& t, const StateVector<double>& x, const InputVector<double>& u) const override {
+      return output_double(t, x, u);
     }
 
     virtual bool isTimeVarying() const override {
@@ -71,7 +83,7 @@ namespace Drake {
 
   private:
     std::function<InputVector<double>(const double&, const StateVector<double> &, const InputVector<double> &)> dynamics_double;
-    std::function<OutputVector<double>(const StateVector<double> &)> output;
+    std::function<OutputVector<double>(const double&, const StateVector<double> &, const InputVector<double> &)> output_double;
 
     // sys info
     std::function<bool()> is_time_varying;
@@ -83,7 +95,7 @@ namespace Drake {
     typedef std::shared_ptr<WrappableSystemInterface> type;
   };
 
-  template<>
+  template <>
   std::shared_ptr<WrappableSystemInterface> cascade(const std::shared_ptr<WrappableSystemInterface> &sys1, const std::shared_ptr<WrappableSystemInterface> &sys2) {
     auto casc_temp = std::make_shared<CascadeSystem<WrappableSystemInterface, WrappableSystemInterface>>(sys1, sys2);
     return std::make_shared<WrappableSystem>(casc_temp);
