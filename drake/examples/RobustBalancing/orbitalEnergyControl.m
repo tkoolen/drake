51ea716b28c0ca77dabd8cc8b5d7cbc971869513
Twan Koolen <koolen.twan@gmail.com>
2016-03-09 13:24:59 -0500

    pseudoinverse + nullspace + univariate quadratic / linear equation approach instead of nonlinear program.

--- drake/examples/RobustBalancing/orbitalEnergyControl.m
+++ drake/examples/RobustBalancing/orbitalEnergyControl.m
@@ -2,8 +2,8 @@ function orbitalEnergyControl()
 checkDependency('spotless');
 checkDependency('snopt');
 
-n = 5;
-%g = msspoly('g');
+n = 3;
+% g = msspoly('g');
 g = 9.8;
 x = msspoly('x');
 xd = msspoly('xd');
@@ -15,10 +15,20 @@ fp = diff(f, x); [ap, pp] = pdecomp(fp, x); ap = ap';
 fpp = diff(fp, x); [app, ppp] = pdecomp(fpp, x); app = app';
 h = f - fp * x; [ah, ph] = pdecomp(h, x); ah = ah';
 fint = integral(f * x, x); [aint, pint] = pdecomp(fint, x); aint = aint';
+
+s = diag(eye(n + 1));
+sp = diag(double(diff([0; ap], a)));
+spp = diag(double(diff([0; 0; app], a)));
+sint = diag(double(diff(aint, a)));
+sh = diag(double(diff([ah(1); 0; ah(2:end)], a)));
+
 orbital_energy = 1/2 * xd^2 * h^2 + g * x^2 * f - 3 * g * fint;
-P = diff(diff(orbital_energy, a)', a);
-q = diff(orbital_energy - 1/2 * a' * P * a, a)';
-valuecheck(double(orbitalEnergy(a, P, q) - orbital_energy), 0);
+S = sh * sh';
+r = g * (s - 3 * sint);
+
+P = xd^2 * (sh .* mons) * (sh .* mons)'; %S .* (mons * mons'); % diff(diff(orbital_energy, a)', a);
+q = x^2 * r .* mons; %diff(orbital_energy - 1/2 * a' * P * a, a)';
+valuecheck(double(clean(orbitalEnergy(a, P, q) - orbital_energy)), 0);
 
 % for E_orbit = 0:
 force_numerator_divided_by_g = h^2 + 2 * fpp * (-x^2 * f + 3 * fint); % quadratic in coeffs of a; want this to be >= 0 on [x0, 0]
@@ -26,46 +36,79 @@ Pf = diff(diff(force_numerator_divided_by_g, a)', a);
 qf = diff(force_numerator_divided_by_g - 1/2 * a' * Pf * a, a)';
 valuecheck(double(qf), zeros(size(qf)));
 
-Mf = a * a' + [0; ap] * [0; ap]' - a * [0; ap]' - [0; ap] * a' - a * [0; 0; app]' - [0; 0; app] * a' + 3 * [0; 0; app] * aint' + 3 * aint * [0; 0; app]'; 
-valuecheck(double(clean(mons' * Mf * mons - force_numerator_divided_by_g)), 0);
-% having M be positive semidefinite requires that a_n = 0 for n >= 3; if n < 0
+%Mf = a * a' + [0; ap] * [0; ap]' - a * [0; ap]' - [0; ap] * a' - a * [0; 0; app]' - [0; 0; app] * a' + 3 * [0; 0; app] * aint' + 3 * aint * [0; 0; app]'; 
+%valuecheck(double(clean(mons' * Mf * mons - force_numerator_divided_by_g)), 0);
+
+
+Sf = s * s' + sp * sp' - s * sp' - sp * s' - s * spp' - spp * s' + 3 * spp * sint' + 3 * sint * spp';
+
+valuecheck(double(clean(mons' * (Sf .* (a * a')) * mons - force_numerator_divided_by_g)), 0);
+
+% having M be positive semidefinite requires that a_n = 0 for n >= 3; if n < 3
 % then it is trivially positive semidefinite
 
-x0 = -0.2;
-z0 = 1;
+x0 = -1;
+z0 = 0.95;
 omega0 = sqrt(g / z0);
-xd0 = -omega0 * x0 * 1.1; %95; %1.05;
+xd0 = -omega0 * x0 * 0.95; %95; %1.05;
 zd0 = 0;
 dzdx0 = zd0 / xd0;
 zf = 1;
-P0 = double(subs(P, [x; xd], [x0; xd0]));
-q0 = double(subs(q, [x; xd], [x0; xd0]));
 
-prog = NonlinearProgram(n + 1);
+C = [double(diff(subs(f, x, x0), a)); double(diff(subs(diff(f, x), x, x0), a)); double(diff(subs(f, x, 0), a))];
+a_particular = C \ [z0; dzdx0; zf];
+N = null(C);
+w = msspoly('w', 1);
+orbital_energy_w = subs(orbital_energy, a, a_particular + N * w);
+R = pdecomp(orbital_energy_w, w);
+R = clean(R);
+R0 = double(subs(R, [x; xd], [x0; xd0]));
+% w = (-R0(2) + sqrt(R0(2)^2 - 4 * R0(3) * R0(1))) / (2 * R0(3));
+% valuecheck(R0(3), 0, 1e-14);
+w = -R0(1) / R0(2);
+a_sol = a_particular + N * w;
 
-orbital_energy_constraint = FunctionHandleConstraint(0, 0, n + 1, @(a) orbitalEnergy(a, P0, q0), 2);
-prog = prog.addNonlinearConstraint(orbital_energy_constraint);
 
-for x_knot = linspace(x0, 0, 2 * n) % just enforce at a couple of sample points
-  Pf_knot = double(subs(Pf, x, x_knot));
-  force_constraint = FunctionHandleConstraint(0, inf, n + 1, @(a) forceConstraint(a, Pf_knot), 2); % only require that initial force >= 0...
-  prog = prog.addNonlinearConstraint(force_constraint);
-end
-
-initial_height_constraint = LinearConstraint(z0, z0, double(diff(subs(f, x, x0), a)));
-prog = prog.addLinearConstraint(initial_height_constraint);
-
-initial_slope_constraint = LinearConstraint(dzdx0, dzdx0, double(diff(subs(diff(f, x), x, x0), a)));
-prog = prog.addLinearConstraint(initial_slope_constraint);
-
-final_height_constraint = LinearConstraint(zf, zf, double(diff(subs(f, x, 0), a)));
-prog = prog.addLinearConstraint(final_height_constraint);
-
-prog = prog.addQuadraticCost(eye(n + 1), zeros(n + 1, 1));
-
-a0 = randn(n + 1, 1);
-a_sol = prog.solve(a0);
-
+% % nonlinear program solution
+% P0 = double(subs(P, [x; xd], [x0; xd0]));
+% q0 = double(subs(q, [x; xd], [x0; xd0]));
+% 
+% % oe = subs(orbital_energy, a(1), z0);
+% % P = diff(diff(orbital_energy, a)', a);
+% % q = diff(orbital_energy - 1/2 * a' * P * a, a)';
+% 
+% prog = NonlinearProgram(n + 2);
+% 
+% orbital_energy_constraint = FunctionHandleConstraint(0, 0, n + 1, @(a) orbitalEnergy(a, P0, q0), 2);
+% prog = prog.addNonlinearConstraint(orbital_energy_constraint, 1 : n + 1);
+% 
+% % for x_knot = linspace(x0, 0, 2 * n) % just enforce at a couple of sample points
+% %   Pf_knot = double(subs(Pf, x, x_knot));
+% %   force_constraint = FunctionHandleConstraint(0, inf, n + 1, @(a) forceConstraint(a, Pf_knot), 2); % only require that initial force >= 0...
+% %   prog = prog.addNonlinearConstraint(force_constraint);
+% % end
+% 
+% initial_height_constraint = LinearConstraint(z0, z0, double(diff(subs(f, x, x0), a)));
+% prog = prog.addLinearConstraint(initial_height_constraint, 1 : n + 1);
+% 
+% initial_slope_constraint = LinearConstraint(dzdx0, dzdx0, double(diff(subs(diff(f, x), x, x0), a)));
+% prog = prog.addLinearConstraint(initial_slope_constraint, 1 : n + 1);
+% 
+% final_height_constraint = LinearConstraint(zf, zf, double(diff(subs(f, x, 0), a)));
+% prog = prog.addLinearConstraint(final_height_constraint, 1 : n + 1);
+% 
+% % Pw = clean(diff(diff(orbital_energy_w, w)', w));
+% % qw = clean(diff(clean(orbital_energy_w - 1/2 * Pw * w^2), w)');
+% 
+% Q = zeros(n + 2, n + 2);
+% Q(1 : n + 1, 1 : n + 1) = eye(n + 1);
+% prog = prog.addQuadraticCost(Q, zeros(n + 2, 1));
+% 
+% a0 = randn(n + 1, 1);
+% lambda0 = randn;
+% sol = prog.solve([a0; lambda0]);
+% a_sol = sol(1 : n + 1);
+% 
 xs = linspace(x0, 0, 100);
 zs = polyval(flipud(a_sol), xs);
 
