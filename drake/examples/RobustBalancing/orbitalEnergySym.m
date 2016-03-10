aecd147ce1586480c9d44149082c35b6cbbe189a
Twan Koolen <koolen.twan@gmail.com>
2016-03-10 14:57:19 -0500

    Simplified height trajectory determination.

--- drake/examples/RobustBalancing/orbitalEnergySym.m
+++ drake/examples/RobustBalancing/orbitalEnergySym.m
@@ -10,17 +10,19 @@ q = [x; z];
 v = [xd; zd];
 q0 = [x0; z0];
 v0 = [xd0; zd0];
+n = 3;
 
-[f_sol, u_sol] = cubicCaptureHeightTrajectory(g, q, v, q0, v0, zf);
+[f_sol, u_sol] = captureHeightTrajectory(g, q, v, q0, v0, zf, n);
 
 vdot_sol = [0; -g] + u_sol * q;
 xdot_sol = simplify([v; vdot_sol]);
+% [nxdot, dxdot] = numden(xdot_sol);
 
 g_val = 9.8;
 x0_val = -1;
-z0_val = 0.95;
+z0_val = 0.75; %0.95;
 omega0 = sqrt(g_val / z0_val);
-xd0_val = -omega0 * x0_val * 0.95; %1.05;
+xd0_val = -omega0 * x0_val * 1.03; %1.05;
 zd0_val = 0;
 zf_val = 1;
 
@@ -31,11 +33,11 @@ zs = polyval(sym2poly(f_val), xs);
 xds = horizontalVelocities(f_val, g_val, xd0_val, xs);
 dzdxs = polyval(sym2poly(diff(f_val, x)), xs);
 zds = dzdxs .* xds;
-us = inputs(u_val, q, v, xs, zs, xds, zds);
+f_legs = legForce(u_val, q, v, xs, zs, xds, zds);
 
 half_index = floor(length(xs) / 2);
 f_half_val = subs(f_sol, [g; x0; z0; xd0; zd0; zf], [g_val; xs(half_index); zs(half_index); xds(half_index); zds(half_index); zf_val]);
-valuecheck(sym2poly(f_val), sym2poly(f_half_val));
+% valuecheck(sym2poly(f_val), sym2poly(f_half_val));
 
 xdot_fun = matlabFunction(subs(xdot_sol, [g; zf], [g_val; zf_val]), 'Vars', [q; v]);
 T = 3 / omega0;
@@ -50,13 +52,13 @@ hold on;
 plot(xs, zs, 'b');
 plot(xtraj(:, 1), xtraj(:, 2), 'r');
 xlabel('q_x'); ylabel('q_z');
-legend({'trajectory', 'simulation'});
+legend({'trajectory', 'simulation'}, 'Location', 'Best');
 % axis(1.2 * [min(x0, 0), max(x0, 0), 0, max([z'; xtraj(:, 2)])]);
 hold off;
 
 subplot(3, 1, 2);
-plot(xs, us);
-xlabel('q_x'); ylabel('u');
+plot(xs, f_legs);
+xlabel('q_x'); ylabel('f_{leg}');
 
 subplot(3, 1, 3);
 plot(xs, xds);
@@ -69,10 +71,9 @@ v_spot = msspoly('v', length(v));
 n = sym2msspoly([q; v], [q_spot; v_spot], n_sym);
 d = sym2msspoly([q; v], [q_spot; v_spot], d_sym);
 
-
 end
 
-function [f_sol, u_sol] = cubicCaptureHeightTrajectory(g, q, v, q0, v0, zf)
+function [f_sol, u_sol] = captureHeightTrajectory(g, q, v, q0, v0, zf, n)
 x = q(1);
 z = q(2);
 xd = v(1);
@@ -82,7 +83,6 @@ z0 = q0(2);
 xd0 = v0(1);
 zd0 = v0(2);
 
-n = 3;
 a = sym('a', [n + 1, 1]);
 assumeAlso(a, 'real');
 
@@ -94,29 +94,53 @@ end
 f = a' * mons;
 fp = diff(f, x);
 fpp = diff(fp, x);
-h = f - fp * x;
-fint = int(f * x, x);
 
-orbital_energy = simplify(1/2 * xd^2 * h^2 + g * x^2 * f - 3 * g * fint);
+A = sym.zeros(4, n + 1);
+A(1, :) = jacobian(subs(f, x, 0), a); % final height
+A(2, :) = jacobian(subs(f, x, x0), a); % initial height
+A(3, :) = jacobian(subs(fp, x, x0), a); % initial slope
+for i = 0 : n
+  A(4, i + 1) = 2 * g * (1 - i) / (i + 2) * x0^(i + 2); % orbital energy
+end
 
-dzdx0 = zd0 / xd0;
+b = [...
+  zf;
+  z0;
+  zd0 / xd0;
+  xd0^2 * (z0 - zd0 / xd0 * x0)^2]; % note: xd0^2 is xd0 in paper, but that's wrong.
 
-C = [...
-  jacobian(subs(f, x, x0), a); 
-  jacobian(subs(fp, x, x0), a);
-  jacobian(subs(f, x, 0), a)];
-a_particular = C \ [z0; dzdx0; zf];
-N = null(C);
-syms w real;
-orbital_energy_w = simplify(subs(orbital_energy, [x; xd; a], [x0; xd0; a_particular + N * w]));
-% [R, m] = coeffs(orbital_energy_w, w);
-w = solve(orbital_energy_w == 0, w); % just a linear equation
-a_sol = simplify(a_particular + N * w);
+a_sol = A \ b;
+
+% h = f - fp * x;
+% fint = int(f * x, x);
+% 
+% orbital_energy = simplify(1/2 * xd^2 * h^2 + g * x^2 * f - 3 * g * fint);
+% 
+% dzdx0 = zd0 / xd0;
+% 
+% C = [...
+%   jacobian(subs(f, x, x0), a); 
+%   jacobian(subs(fp, x, x0), a);
+%   jacobian(subs(f, x, 0), a)];
+% a_particular = C \ [z0; dzdx0; zf];
+% N = null(C);
+% syms w real;
+% orbital_energy_w = simplify(subs(orbital_energy, [x; xd; a], [x0; xd0; a_particular + N * w]));
+% % [R, m] = coeffs(orbital_energy_w, w);
+% w = solve(orbital_energy_w == 0, w); % just a linear equation
+% a_sol = simplify(a_particular + N * w);
+
 f_sol = simplify(subs(f, a, a_sol));
-
 u = (g + fpp * xd^2) / (z - fp * x);
 u_sol = simplify(subs(u, a, a_sol));
 u_sol = simplify(subs(u_sol, [x0; z0; xd0; zd0], [x; z; xd; zd]));
+
+% % reason why there's no quadratic term:
+% ah = coeffs(h, x)';
+% sh = diag(double(jacobian([ah(1); 0; ah(2:end)], a)));
+% assert(all(all(simplify(simplify(xd^2 * (sh .* mons) * (sh .* mons)' - jacobian(jacobian(orbital_energy, a)', a)) == 0))));
+% assert(logical(simplify(dot(N, sh .* subs(mons, x, x0))) == 0));
+
 
 % ap = coeffs(fp, x)';
 % app = coeffs(fpp, x)';
@@ -160,9 +184,9 @@ xdsquared_denominator_val = polyval(sym2poly(xdsquared_denominator), xs);
 xds = abs(sqrt(xdsquared_numerator_val ./ xdsquared_denominator_val)) * sign(xd0);
 end
 
-function us = inputs(u, q, v, xs, zs, xds, zds)
-fun = matlabFunction(u, 'Vars', [q; v]);
-us = fun(xs, zs, xds, zds);
+function f_legs = legForce(u, q, v, xs, zs, xds, zds)
+fun = matlabFunction(u * norm(q), 'Vars', [q; v]);
+f_legs = fun(xs, zs, xds, zds);
 end
 
 function spot_poly = sym2msspoly(sym_vars, spot_vars, sym_poly)
