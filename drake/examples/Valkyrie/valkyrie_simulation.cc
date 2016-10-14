2aaf93b06a77db9e477064b600eee44a8d32d5b3
Twan Koolen <koolen.twan@gmail.com>
2016-10-14 17:32:44 -0400

    valkyrie_simulation WIP.

--- drake/examples/Valkyrie/valkyrie_simulation.cc
+++ drake/examples/Valkyrie/valkyrie_simulation.cc
@@ -1,47 +1,49 @@
 #include <memory>
 
-#include <lcm/lcm-cpp.hpp>
 #include <drake/lcm/drake_lcm.h>
+#include <lcm/lcm-cpp.hpp>
 
 #include "lcmtypes/bot_core/atlas_command_t.hpp"
 #include "lcmtypes/bot_core/robot_state_t.hpp"
 
-#include "drake/systems/framework/primitives/pass_through.h"
+#include "actuator_effort_to_rigid_body_plant_input_converter.h"
 #include "drake/common/drake_path.h"
 #include "drake/common/text_logging.h"
+#include "drake/examples/Valkyrie/robot_state_encoder.h"
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/framework/primitives/constant_vector_source.h"
+#include "drake/systems/framework/primitives/pass_through.h"
 #include "drake/systems/lcm/lcm_publisher_system.h"
 #include "drake/systems/lcm/lcm_subscriber_system.h"
 #include "drake/systems/lcm/lcmt_drake_signal_translator.h"
 #include "drake/systems/plants/parser_urdf.h"
 #include "drake/systems/plants/rigid_body_plant/rigid_body_plant.h"
-#include "drake/examples/Valkyrie/robot_state_encoder.h"
+#include "simple_joint_sensors.h"
 #include "simple_robot_command_to_desired_effort_converter.h"
 
 // TODO: use syntactic sugar for connections from cars-simulator2 branch
 
 namespace drake {
-namespace examples {
-namespace valkyrie {
+namespace systems {
 
 using std::move;
+using std::unique_ptr;
 using std::make_unique;
-using systems::RigidBodyPlant;
-using systems::lcm::LcmPublisherSystem;
-using systems::lcm::LcmSubscriberSystem;
-using systems::lcm::LcmtDrakeSignalTranslator;
-using systems::DiagramContext;
-using lcm::DrakeLcm;
+using std::map;
+
 using Eigen::VectorXd;
-using drake::systems::plants::joints::kRollPitchYaw;
 using bot_core::atlas_command_t;
 using bot_core::robot_state_t;
 
-int main(int argc, const char **argv) {
+using drake::lcm::DrakeLcm;
+using lcm::LcmSubscriberSystem;
+using lcm::LcmPublisherSystem;
+using plants::joints::kRollPitchYaw;
+
+int main(int argc, const char** argv) {
   drake::log()->set_level(spdlog::level::trace);
-  auto builder = std::make_unique<systems::DiagramBuilder<double>>();
+  auto builder = std::make_unique<DiagramBuilder<double>>();
 
   // Create RigidBodyTree.
   auto tree_ptr = make_unique<RigidBodyTree>();
@@ -55,47 +57,97 @@ int main(int argc, const char **argv) {
   RigidBodyPlant<double> plant(move(tree_ptr));
   const auto& tree = plant.get_rigid_body_tree();
 
+  // RigidBodyActuators.
+  std::vector<const RigidBodyActuator*> actuators;
+  for (const auto& actuator : tree.actuators) {
+    actuators.push_back(&actuator);
+  }
+  // Currently, all RigidBodyActuators are assumed to be one-dimensional.
+  const int actuator_effort_length = 1;
+
   // LCM communication.
   DrakeLcm lcm;
 
   // LCM inputs.
-  auto atlas_command_subscriber = LcmSubscriberSystem::Make<atlas_command_t>("ATLAS_COMMAND", &lcm);
-  std::vector<const RigidBodyActuator*> rigid_body_actuator_ptrs;
-  for (const auto& actuator : tree.actuators) {
-    rigid_body_actuator_ptrs.push_back(&actuator);
+  auto atlas_command_subscriber =
+      LcmSubscriberSystem::Make<atlas_command_t>("ATLAS_COMMAND", &lcm);
+  SimpleRobotCommandToDesiredEffortConverter
+      robot_command_to_desired_effort_converter(actuators);
+
+  // Placeholder for actuator dynamics.
+  map<const RigidBodyActuator*, unique_ptr<System<double>>> actuator_dynamics;
+  for (const auto& actuator : actuators) {
+    actuator_dynamics.emplace(std::make_pair(
+        actuator, make_unique<PassThrough<double>>(actuator_effort_length)));
   }
-  SimpleRobotCommandToDesiredEffortConverter robot_command_to_desired_effort_converter(rigid_body_actuator_ptrs);
+
+  // Conversion from desired efforts to RigidBodyPlant input vector.
+  ActuatorEffortToRigidBodyPlantInputConverter<double>
+      actuator_effort_to_rigid_body_plant_input_converter(actuators);
+
+  // Placeholder for effort sensors.
+  map<const RigidBodyActuator*, unique_ptr<System<double>>> effort_sensors;
+  for (const auto& actuator : actuators) {
+    effort_sensors.emplace(std::make_pair(
+        actuator, make_unique<PassThrough<double>>(actuator_effort_length)));
+  }
 
   // LCM outputs.
   RobotStateEncoder robot_state_encoder(plant.get_rigid_body_tree());
-  auto robot_state_publisher = LcmPublisherSystem::Make<robot_state_t>("EST_ROBOT_STATE", &lcm);
-
-  // Placeholder for actuator dynamics.
-  systems::PassThrough<double> actuators(plant.get_input_size());
-
-  // Placeholder for effort sensors
-  systems::PassThrough<double> effort_sensors(plant.get_input_size());
-
-  // TODO: Joint sensors.
+  auto robot_state_publisher =
+      LcmPublisherSystem::Make<robot_state_t>("EST_ROBOT_STATE", &lcm);
 
   // TODO: Force/torque sensors.
 
-  // Connections
-  builder->Connect(*atlas_command_subscriber, robot_command_to_desired_effort_converter);
-  builder->Connect(robot_command_to_desired_effort_converter, actuators);
-  builder->Connect(actuators, plant);
-  builder->Connect(actuators, effort_sensors);
-  builder->Connect(plant.get_output_port(0), joint_sensors.get_kinematics_results_port());
-  builder->Connect(joint_sensors.get_configuration_port(), robot_state_encoder.get_configuration_port());
-  builder->Connect(joint_sensors.get_velocity_port(), robot_state_encoder.get_velocity_port());
-  builder->Connect(effort_sensors.get_output_port(0), robot_state_encoder.get_effort_port());
+
+  // Connections.
+  // LCM message to desired effort conversion.
+  builder->Connect(*atlas_command_subscriber,
+                   robot_command_to_desired_effort_converter);
+
+  for (const auto& actuator : actuators) {
+    // Desired effort inputs to actuator dynamics.
+    const auto& desired_effort_output =
+        robot_command_to_desired_effort_converter
+            .get_desired_effort_output_port(*actuator);
+    const auto& desired_effort_input =
+        actuator_dynamics.at(actuator)->get_input_port(0);
+    builder->Connect(desired_effort_output, desired_effort_input);
+
+    // Efforts to effort sensors.
+    const auto& effort_output_port =
+        actuator_dynamics.at(actuator)->get_output_port(0);
+    const auto& measured_effort_input_port =
+        effort_sensors.at(actuator)->get_input_port(0);
+    builder->Connect(effort_output_port, measured_effort_input_port);
+
+    // Efforts to rigid body plant input
+    builder->Connect(effort_output_port,
+                     actuator_effort_to_rigid_body_plant_input_converter
+                         .get_effort_input_port(*actuator));
+    // TODO: can you connect one output port to multiple input ports?
+
+    // Effort sensors to robot state encoder.
+    const auto& measured_effort_output_port = effort_sensors.at(actuator)->get_output_port(0);
+    const auto& state_encoder_effort_input_port = robot_state_encoder.get_effort_port(*actuator);
+    builder->Connect(measured_effort_output_port, state_encoder_effort_input_port);\
+  }
+
+  // Plant input to plant.
+  builder->Connect(actuator_effort_to_rigid_body_plant_input_converter, plant);
+
+  // Kinematics results to robot state encoder.
+  builder->Connect(plant.get_output_port(1), robot_state_encoder.get_kinematics_results_port()); // FIXME: magic number
+
   // TODO: design and connect wrench ports.
+
+  // Robot state encoder to robot state publisher.
   builder->Connect(robot_state_encoder, *robot_state_publisher);
 
   auto diagram = builder->Build();
 
   // Create simulator.
-  auto simulator = std::make_unique<systems::Simulator<double>>(*diagram);
+  auto simulator = std::make_unique<Simulator<double>>(*diagram);
   simulator->Initialize();
   auto context = simulator->get_mutable_context();
 
@@ -110,10 +162,9 @@ int main(int argc, const char **argv) {
   }
 }
 
-}  // valkyrie
-}  // examples
+}  // systems
 }  // drake
 
 int main(int argc, const char* argv[]) {
-  return drake::examples::valkyrie::main(argc, argv);
+  return drake::systems::main(argc, argv);
 }
