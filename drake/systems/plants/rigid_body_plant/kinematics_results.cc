6e99f2e2765e7b4588b5ad39467c79d9e8b5b3ca
Twan Koolen <koolen.twan@gmail.com>
2016-10-14 15:23:24 -0400

    Add joint position and velocity getters to KinematicsResults, as well as methods for getting pose and twist with respect to world.
    clang-format.

--- drake/systems/plants/rigid_body_plant/kinematics_results.cc
+++ drake/systems/plants/rigid_body_plant/kinematics_results.cc
@@ -6,50 +6,64 @@
 namespace drake {
 namespace systems {
 
-template<typename T>
-KinematicsResults<T>::KinematicsResults(const RigidBodyTree &tree) :
-    tree_(tree), kinematics_cache_(tree_.bodies) {
-}
+template <typename T>
+KinematicsResults<T>::KinematicsResults(const RigidBodyTree& tree)
+    : tree_(tree), kinematics_cache_(tree_.bodies) {}
 
-template<typename T>
+template <typename T>
 int KinematicsResults<T>::get_num_bodies() const {
   return tree_.get_num_bodies();
 }
 
-template<typename T>
+template <typename T>
 int KinematicsResults<T>::get_num_positions() const {
   return kinematics_cache_.get_num_positions();
 }
 
-template<typename T>
+template <typename T>
 int KinematicsResults<T>::get_num_velocities() const {
   return kinematics_cache_.get_num_velocities();
 }
 
-template<typename T>
+template <typename T>
 Quaternion<T> KinematicsResults<T>::get_body_orientation(int body_index) const {
-  Isometry3<T>
-      pose = tree_.relativeTransform(kinematics_cache_, 0, body_index);
+  Isometry3<T> pose = tree_.relativeTransform(kinematics_cache_, 0, body_index);
   Vector4<T> quat_vector = drake::math::rotmat2quat(pose.linear());
   // Note that Eigen quaternion elements are not laid out in memory in the
   // same way Drake currently aligns them. See issue #3470.
-  return Quaternion<T>(
-      quat_vector[0], quat_vector[1], quat_vector[2], quat_vector[3]);
+  return Quaternion<T>(quat_vector[0], quat_vector[1], quat_vector[2],
+                       quat_vector[3]);
 }
 
-template<typename T>
+template <typename T>
 Vector3<T> KinematicsResults<T>::get_body_position(int body_index) const {
-  Isometry3<T>
-      pose = tree_.relativeTransform(kinematics_cache_, 0, body_index);
+  Isometry3<T> pose = tree_.relativeTransform(kinematics_cache_, 0, body_index);
   return pose.translation();
 }
 
-template<typename T>
-void KinematicsResults<T>::UpdateFromContext(const Context<T> &context) {
+template <typename T>
+Isometry3<T> KinematicsResults<T>::get_pose_in_world(
+    const RigidBody& body) const {
+  const auto& world = tree_.world();
+  return tree_.relativeTransform(kinematics_cache_, world.get_body_index(),
+                                 body.get_body_index());
+}
+
+template <typename T>
+TwistVector<T> KinematicsResults<T>::get_twist_with_respect_to_world(
+    const RigidBody& body) const {
+  const auto& world = tree_.world();
+  return tree_.relativeTwist(kinematics_cache_, world.get_body_index(),
+                             body.get_body_index(), 0);
+}
+
+template <typename T>
+void KinematicsResults<T>::UpdateFromContext(const Context<T>& context) {
   // TODO(amcastro-tri): provide nicer accessor to an Eigen representation for
   // LeafSystems.
-  auto x = dynamic_cast<const BasicVector<T> &>(
-      context.get_continuous_state()->get_state()).get_value();
+  auto x = dynamic_cast<const BasicVector<T>&>(
+               context.get_continuous_state()->get_state())
+               .get_value();
 
   const int nq = tree_.get_num_positions();
   const int nv = tree_.get_num_velocities();
@@ -64,9 +78,22 @@ void KinematicsResults<T>::UpdateFromContext(const Context<T> &context) {
   tree_.doKinematics(kinematics_cache_, false);
 }
 
+template <typename T>
+Eigen::VectorBlock<const VectorX<T>> KinematicsResults<T>::get_joint_position(
+    const RigidBody& body) const {
+  return kinematics_cache_.getQ().segment(body.get_position_start_index(),
+                                          body.getJoint().get_num_positions());
+}
+
+template <typename T>
+Eigen::VectorBlock<const VectorX<T>> KinematicsResults<T>::get_joint_velocity(
+    const RigidBody& body) const {
+  return kinematics_cache_.getV().segment(body.get_velocity_start_index(),
+                                          body.getJoint().get_num_velocities());
+}
+
 // Explicitly instantiates on the most common scalar types.
 template class DRAKE_EXPORT KinematicsResults<double>;
 
 }  // namespace systems
 }  // namespace drake
-
